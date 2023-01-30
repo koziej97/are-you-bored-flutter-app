1d5864243866db604f2a8dcc8cfd0542cca786f1
koziej97 <lukkoz2@st.amu.edu.pl>
2023-01-30 02:07:16 +0100

    added sliding up panel to HomeScreen

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
+import 'sliding_up_panel_screen.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -63,31 +65,8 @@ class HomeScreen extends StatelessWidget {
           maxHeight: 300,
           borderRadius: const BorderRadius.only(
               topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-          panel: Stack(fit: StackFit.expand, children: [
-            Container(
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const Padding(padding: EdgeInsets.only(top: 15.0)),
-                      Text(
-                        "Swipe up to specify activity type",
-                        style: GoogleFonts.lato(
-                            textStyle: const TextStyle(
-                                fontSize: 15.0,
-                                height: 1.2,
-                                color: Colors.black,
-                                fontWeight: FontWeight.w400)),
-                      ),
-                      const Padding(
-                          padding: EdgeInsets.only(
-                              top: 10.0, left: 100.0, right: 100.0, bottom: 10.0),
-                          child: Divider(
-                            height: 10,
-                            color: Colors.black,
-                            thickness: 1,
-                          )),
-                    ]))
+          panel: Stack(fit: StackFit.expand, children: const [
+            SlidingUpPanelScreen()
           ]))
     ]));
   }
