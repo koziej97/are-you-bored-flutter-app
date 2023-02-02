70bd778f8d50addf305adf96c8b914445ce36828
koziej97 <lukkoz2@st.amu.edu.pl>
2023-02-02 19:17:45 +0100

    added ActivityProvider and ActivityRepository

---
+++ lib/data/data_provider/activity_provider.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart' as http;
+
+class ActivityProvider {
+  static const String _baseUrl = "http://www.boredapi.com/api/activity/";
+
+  static Future<String> getRandomActivity() async {
+    final response = await http.get(Uri.parse(_baseUrl));
+    if (response.statusCode == 200){
+      return response.body;
+    } else {
+      return "error";
+    }
+  }
+}
--- lib/data/model/activity_model.dart
+++ lib/data/model/activity_model.dart
@@ -10,13 +10,25 @@ class ActivityModel {
   String type;
   int participants;
   String link;
+  int backgroundType = 0;
 
   factory ActivityModel.fromJson(Map<String, dynamic> json){
     return ActivityModel(
         activity: json["activity"],
         type: json["type"],
         participants: json["participants"],
-        link: json["link"] ?? ""
+        link: json["link"] ?? "",
     );
+  }
+
+  // TODO - now it's not in use
+  setupBackgroundType(ActivityModel activityModel) {
+    if (activityModel.type == "education" || activityModel.type == "cooking" || activityModel.type == "recreational"){
+      activityModel.backgroundType = 0;
+    } else if (activityModel.type == "music" || activityModel.type == "diy" || activityModel.type == "relaxation"){
+      activityModel.backgroundType = 1;
+    } else {
+      activityModel.backgroundType = 2;
+    }
   }
 }
