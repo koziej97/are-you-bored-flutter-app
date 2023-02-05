98104dfd9d2b5e4265a9f10d8faf689df8ec00f0
koziej97 <lukkoz2@st.amu.edu.pl>
2023-02-05 19:50:59 +0100

    added bloc classes

---
+++ lib/bloc/activity_bloc/activity_state.dart
@@ -0,0 +1,16 @@
+part of 'activity_bloc.dart';
+
+@immutable
+abstract class ActivityState {}
+
+class ActivityInitial extends ActivityState {}
+
+class ActivityLoadingState extends ActivityState {}
+
+class ActivityLoadedState extends ActivityState {
+  final ActivityModel activity;
+
+  ActivityLoadedState(this.activity);
+}
+
+class ActivityErrorState extends ActivityState {}
