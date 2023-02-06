a5eeb83158560191a9655a285afa29a3ddc1b34a
koziej97 <lukkoz2@st.amu.edu.pl>
2023-02-06 16:38:35 +0100

    locked portrait orientation in whole app

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:are_you_bored/bloc/activity_bloc/activity_bloc.dart';
 import 'package:are_you_bored/data/repository/activity_repository.dart';
 import 'package:are_you_bored/presentation/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
@@ -19,6 +20,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       title: 'Are you bored?',
       theme: ThemeData(
